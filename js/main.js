130b819e77c44c1610337dfc3286c9f287a0fd45
Foxy3555 <sema12301@gmail.com>
2024-04-22 18:58:07 +0700

    Add chapter 8

--- js/main.js
+++ js/main.js
@@ -2,24 +2,27 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
+        brand: 'Vue Mastery',
         description: "A pair of warm, fuzzy socks",
         altText: "A pair of socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        selectedVariant: 0,
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: false,
-        inventory: 0,
-        onSale: "On Sale",
+        inventory: 100,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg"
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                variantOnSale: true
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
+                variantOnSale: false
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -31,12 +34,33 @@ let app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
         delToCart() {
             this.cart -= 1
         },
 
+
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.variants[this.selectedVariant].variantOnSale === true) {
+                return 'On Sale!'
+            } else {
+                return '';
+            }
+        },
     }
+
 })
