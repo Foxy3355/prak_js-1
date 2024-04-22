6ad1b1cd0df153920d40673387126b25e6d548ed
Foxy3555 <sema12301@gmail.com>
2024-04-22 10:32:02 +0700

    Add chapter 4

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks",
         altText: "A pair of socks",
         image: "./assets/vmSocks-green-onWhite.jpg",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 10,
+        onSale: "On Sale"
     }
  })
  
